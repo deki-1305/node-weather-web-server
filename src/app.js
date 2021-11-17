a126b0645a43cc4915e2395b1ef5d7b5e84e24a4
deki-1305 <dejanpelagic@gmail.com>
2021-11-17 19:00:46 +0100

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -75,6 +76,6 @@ app.get('*', (req, res) => {
     name: 'Dejan Pelagic'})
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000.');
+app.listen(port, () => {
+    console.log('Server is running on port ' + port);
 })
